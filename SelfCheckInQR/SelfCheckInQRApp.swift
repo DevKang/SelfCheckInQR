c9e82ee0e1618d4cee22394a14e16342d1c69d33
hyunho <mizzking75@gmail.com>
2022-03-10 20:58:35 +0900

    Initial Commit

---
+++ SelfCheckInQR/SelfCheckInQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfCheckInQRApp.swift
+//  SelfCheckInQR
+//
+//  Created by hyunho lee on 2022/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SelfCheckInQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
